cb5d0c814a4e3a9320829295c67af65b2bdcd55c
graydonhope <41659296+graydonhope@users.noreply.github.com>
2018-10-22 21:54:46 -0400

    Adding finished product

--- Block.java
+++ Block.java
@@ -55,7 +55,7 @@ public class Block {
 	public String getPreviousHash(){
 		return this.previousHash;
 	}
-
+	
 	public void setHash(String hash){
 		this.hash = hash;
 	}
--- BlockChain.java
+++ BlockChain.java
@@ -8,41 +8,25 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-
+@SuppressWarnings("unchecked")
 
 public class BlockChain {
-
 	private ArrayList<Block> blocks;
 
+
+	/**
+	 * Entry point of the Java program. Calls the startUp method to begin the block chain validation/transaction process
+	 * @param {String[]} args [Takes input from command line]
+	 */
 	public static void main(String[] args){
-		
-		//1. Read blockchain from file
-		BlockChain blockChain = fromFile("C:\\Users\\Graydon\\Documents\\School\\University of Ottawa\\Second Year\\Data Structures and Algorithms - CSI 2110\\CSI_Asignment2\\bitCoinBank.txt");		
-		
-		//2. Validate blockchain
-		boolean blockChainValidated = blockChain.validateBlockChain();
-		System.out.println("Block chain is validated?: " + blockChainValidated);
-
-		//3. Prompt user for new transaction and verify it
-		if(blockChainValidated){
-			System.out.println("Your current block chain was validated!");
-			blockChain.addTransactionToChain(blockChain);
-		}
-
-		blockChain.toFile("C:\\Users\\Graydon\\Documents\\School\\University of Ottawa\\Second Year\\Data Structures and Algorithms - CSI 2110\\CSI_Asignment2\\test.txt");
-
-
-		/*
-		Printing all senders and receivers of blockchain
-		int size = blockChain.blocks.size();
-		for(int i = 0; i < size; i++){
-			System.out.println("Sender:  " + blockChain.blocks.get(i).getTransaction().getSender() + " Receiver: " + blockChain.blocks.get(i).getTransaction().getReceiver());
-		}
-
-		*/
-
+		startUp();
 	}
+	
 
+	/**
+	 * BlockChain constructor which takes a Block object and adds it to the block chain.
+	 * @param {Block} block [The initial block to be added to the chain]
+	 */
 	public BlockChain(Block block){
 		if(block != null){
 			blocks = new ArrayList();			
@@ -54,6 +38,89 @@ public class BlockChain {
 		}
 	}
 
+
+	/**
+	 * Starting method for main. First prompts the user for the file name, then validates the blockchain, then asks 
+	 * if the user would like to add more transactions to the block chain
+	 *  
+	 */
+	public static void startUp(){
+		String bitcoinBankFilename;
+		boolean untilValidFileName = false;
+		BlockChain blockChain;
+		while(!untilValidFileName){
+
+			try{
+				bitcoinBankFilename = promptUserForFilename();
+				blockChain = fromFile(bitcoinBankFilename + ".txt");	
+
+				//2. Validate blockchain
+				boolean blockChainValidated = blockChain.validateBlockChain();
+
+				//3. Prompt user for new transaction and verify it
+				if(blockChainValidated){
+					System.out.println("Your current block chain was validated!");
+					System.out.println("Would you like to add more transactions? ");
+					boolean validAnswer = false;
+					while(!validAnswer){
+						Scanner scanner  = new Scanner(System.in);
+						String userAnswer = scanner.nextLine();
+						if(userAnswer.equals("yes")){
+							validAnswer = true;
+							blockChain.addTransactionToChain(blockChain);
+						}
+						else if(userAnswer.equals("no")){
+							validAnswer = true;
+							untilValidFileName = true;
+							break;
+						}
+						else{
+							validAnswer = false;
+							System.out.println("Invalid input");
+						}
+					}
+					untilValidFileName = true;
+					//6. Add to textfile regardless of new transaction being added
+					blockChain.toFile(bitcoinBankFilename);
+				}
+				else{
+					System.out.println("Block chain was not validated");
+				}
+			}
+			catch(FileNotFoundException e){
+				System.out.println("Illegal file name");
+			}
+		}
+	}
+
+
+	/**
+	 * Prompts user for the file name to read block chain information from
+	 * @return {String} File name value of the file to parse through
+	 */
+	public static String promptUserForFilename(){
+		boolean validSenderInput = false;
+		String senderInput;
+		Scanner scannerInput = new Scanner(System.in);
+		while(!validSenderInput){
+			System.out.println("Please enter the bitcoin bank file name you would like to read from (please do not include '.txt' at the end of file name)");
+			senderInput = scannerInput.nextLine();
+			if(senderInput.equals("")){
+				System.out.println("Invalid file");
+			}
+			else{
+				validSenderInput = true;
+				return senderInput;
+			}
+		}
+		return null;
+	}
+
+
+	/**
+	 * Prompts user for the desired sender of the transaction
+	 * @return {String} Username of the sending party of the transaction
+	 */
 	public static String promptUserForSenderInput(){
 		boolean validSenderInput = false;
 		String senderInput;
@@ -72,77 +139,11 @@ public class BlockChain {
 		return null;
 	}
 
-	public void addTransactionToChain(BlockChain blockChain){
-		int amountToSend;
-		String senderInput, receiverInput, userAnswer;
-		Scanner scannerInput = new Scanner(System.in);
-		boolean addAnotherTransaction = true, validInput = true;
 
-		while(addAnotherTransaction){
-			senderInput = blockChain.promptUserForSenderInput();
-			receiverInput = blockChain.promptUserForReceiverInput();
-			amountToSend = blockChain.promptUserForAmount();
-			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-			Transaction transaction = new Transaction(senderInput, receiverInput, amountToSend);
-			int incrementedIndex;
-			String previousHash;
-
-			//4. Adding transaction to blockchain
-			if(blockChain.getBalance(senderInput) >= amountToSend){
-				int currentBlockChainLength = blockChain.blocks.size();
-
-				if(currentBlockChainLength == 0){
-					incrementedIndex = 0;
-					previousHash = "00000";
-				}
-				else{
-					incrementedIndex = blockChain.blocks.get(currentBlockChainLength - 1).getIndex() + 1;
-					previousHash 	 = blockChain.blocks.get(currentBlockChainLength - 1).getPreviousHash();
-				}
-
-				//Fix creating the timestamp to proper "current" time. System.getTime() or something. for now use currentime in milli
-				String nonce = blockChain.generateNonce(Long.toString(timestamp.getTime()), transaction.toString(), previousHash);
-
-				//Make hash with all new strings
-				try{
-
-					String hash = Sha1.hash(Long.toString(timestamp.getTime()) + ":" + transaction.toString() + "." + nonce + previousHash);
-					System.out.println(hash);
-					Block newBlock  = new Block(incrementedIndex, timestamp, transaction, nonce, previousHash, hash);
-					blockChain.add(newBlock);
-					validInput = false;
-
-					while(!validInput){
-						validInput = false;
-						System.out.println("Would you like to add another Transaction?" + "\n" + 
-										"Please enter yes or no");
-						userAnswer = scannerInput.nextLine();
-						if(userAnswer.equals("yes")){
-							validInput = true;
-							addAnotherTransaction = true;
-						}
-						else if(userAnswer.equals("no")){
-							validInput = true;
-							addAnotherTransaction = false;
-						}
-						else{
-							System.out.println("Invalid entry. Please try again");{
-								validInput = false;
-							}
-						}
-					}
-				}
-				catch(UnsupportedEncodingException e){
-					e.printStackTrace();
-				}
-			}
-			else{
-				System.out.println("Insufficient Funds");
-			}
-		}
-	}
-
-
+	/**
+	 * Prompts the user for the desired receiver of the transaction
+	 * @return {String} Username of the receiving party of the transaction
+	 */
 	public static String promptUserForReceiverInput(){
 		boolean validReceiverInput = false;
 		String receiverInput;
@@ -162,6 +163,11 @@ public class BlockChain {
 		return null;
 	}
 
+
+	/**
+	 * Prompts the user for the desired transaction amount
+	 * @return {int} BitCoin value of transaction
+	 */
 	public static int promptUserForAmount(){
 		int transactionAmount = 0;
 		boolean validAmount = false;
@@ -188,7 +194,88 @@ public class BlockChain {
 	}
 
 
-	public static BlockChain fromFile(String filename){
+	/**
+	 * Allows user to add transactions to the blockChain. The user can add the desired amount of validated transactions 
+	 * @param {BlockChain} blockChain [The BlockChain which transactions are being added]
+	 */
+	public void addTransactionToChain(BlockChain blockChain){
+		int amountToSend;
+		String senderInput, receiverInput, userAnswer;
+		Scanner scannerInput = new Scanner(System.in);
+		boolean addAnotherTransaction = true, validInput = true;
+
+		while(addAnotherTransaction){
+			senderInput = blockChain.promptUserForSenderInput();
+			receiverInput = blockChain.promptUserForReceiverInput();
+			amountToSend = blockChain.promptUserForAmount();
+			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+			Transaction transaction = new Transaction(senderInput, receiverInput, amountToSend);
+			int incrementedIndex;
+			String previousHash;
+
+			//4. Adding transaction to blockchain
+			if(blockChain.getBalance(senderInput) >= amountToSend){
+				int currentBlockChainLength = blockChain.blocks.size();
+
+				if(currentBlockChainLength == 0){
+					incrementedIndex = 0;
+					previousHash = "00000";
+				}
+				else{
+					incrementedIndex = blockChain.blocks.get(currentBlockChainLength - 1).getIndex() + 1;
+					previousHash 	 = blockChain.blocks.get(currentBlockChainLength - 1).getHash();
+				}
+
+				String nonce = blockChain.generateNonce(timestamp.toString(), transaction.toString(), previousHash);
+
+				//Make hash with all new strings
+				try{
+
+					String hash = Sha1.hash(timestamp.toString() + ":" + transaction.toString() + "." + nonce + previousHash);
+					Block newBlock  = new Block(incrementedIndex, timestamp, transaction, nonce, previousHash, hash);
+					blockChain.add(newBlock);
+					validInput = false;
+
+					while(!validInput){
+						validInput = false;
+						System.out.println("Would you like to add another Transaction?" + "\n" + 
+										"Please enter yes or no");
+						userAnswer = scannerInput.nextLine();
+						if(userAnswer.equals("yes")){
+							validInput = true;
+							addAnotherTransaction = true;
+						}
+						else if(userAnswer.equals("no")){
+							validInput = true;
+							addAnotherTransaction = false;
+							//toFile();
+						}
+						else{
+							System.out.println("Invalid entry. Please try again");{
+								validInput = false;
+							}
+						}
+					}
+				}
+				catch(UnsupportedEncodingException e){
+					e.printStackTrace();
+				}
+			}
+			else{
+				System.out.println("Insufficient Funds");
+			}
+		}
+	}
+
+
+	/**
+	 * Reads and adds the blockchain values from the file given. Scans through all the information of the block while adding 
+	 * values to a hash map. After the Scanner has retrieved all block information, it generates a new block and adds to the chain.
+	 * @param {String} filename [Reads block chain information from this file]
+	 * @return {BlockChain} The new BlockChain object created after parsing through file
+	 * @throws FileNotFoundException [description]
+	 */
+	public static BlockChain fromFile(String filename) throws FileNotFoundException{
 		//Read file and add blocks into ArrayList
 		//filename is path location of file
 		HashMap<Integer, String> fileValuesMap = new HashMap();
@@ -199,7 +286,7 @@ public class BlockChain {
 		File file;
 
 		if(filename == null){
-			throw new IllegalArgumentException("File path cannot be null");
+			throw new IllegalArgumentException("File name cannot be null");
 		}
 
 		try{
@@ -256,8 +343,8 @@ public class BlockChain {
 			}
 
 			catch(FileNotFoundException e){
-				e.printStackTrace();
-				return null;
+				throw new FileNotFoundException();
+				//return null;
 			}
 		}
 		catch(NullPointerException e){
@@ -267,15 +354,30 @@ public class BlockChain {
 	}
 
 
+	/**
+	 * Returns the previous Blocks hash value
+	 * @param {BlockChain} blockChain [Current blockChain object to access static information]
+	 * @param {int} blockNumber [Current block index]
+	 * @return {String} value of the previous hash
+	 */
 	public static String getPreviousHash(BlockChain blockChain, int blockNumber){
 		return blockChain.blocks.get(blockNumber - 1).getHash();
 	}
 
+
+	/**
+	 * Adds the Block given to the ArrayList of Blocks at the next available index
+	 * @param {Block} block [Block to add to the chain]
+	 */
 	public void add(Block block){
 		blocks.add(block.getIndex(), block);
 	}
 
 
+	/**
+	 * Writes the blockChain values to the desired text file
+	 * @param {String} filename [The desired file name the user has selected]
+	 */
 	public void toFile(String filename){
 		ArrayList<Block> blockList = this.getBlocks();
 		int size = blockList.size();
@@ -283,7 +385,7 @@ public class BlockChain {
 		PrintWriter printWriter;
 
 		try{
-			file = new File(filename);
+			file = new File(filename + "_ghope049.txt");
 			try{
 				printWriter = new PrintWriter(file, "UTF-8");
 				
@@ -317,8 +419,11 @@ public class BlockChain {
 	}
 
 
+	/**
+	 * Iterates through each Block in the chain and ensures correct values for index, previous hash, and current hash.
+	 * @return {boolean} determining whether the blockChain was successfully validated 
+	 */
 	public boolean validateBlockChain(){
-		//Compare hash and the specific block.toString hash given with the nonce concatenated (for all blocks in blockChain)
 		int numberOfBlocks = blocks.size();
 		String toStringHashValue;
 		boolean isValid = true;
@@ -379,11 +484,20 @@ public class BlockChain {
 	}
 
 
+	/**
+	 * Get method for the blockChain
+	 * @return {ArrayList<Block>} returns the blockChain
+	 */
 	public ArrayList<Block> getBlocks(){
 		return this.blocks;
 	}
 
 
+	/**
+	 * Returns the balance of specified user in the blockchain
+	 * @param {String} username [Checks balance of this user]
+	 * @return {int} balance amount of specified user
+	 */
 	public int getBalance(String username){
 		int amount = 0;
 		int blockChainSize = blocks.size();
@@ -403,21 +517,19 @@ public class BlockChain {
 		return amount;
 	}
 
-	public String toString(BlockChain blockChain){
-		int size = blockChain.blocks.size();
 
-
-		for(int i = 0; i < size; i++){
-
-			
-		}
-		return null;
-	}
-
+	/**
+	 * Generates a nonce which will provide a valid hash code after encrypted through Sha1
+	 * @param {String} timestamp [The timestamp value for transaction]
+	 * @param {String} transaction [String value of the transaction object]
+	 * @param {String} previousHash [Previous hash of the block chain]
+	 * @return {String} value of the generated nonce which gives valid hash code
+	 */
 	public String generateNonce(String timestamp, String transaction, String previousHash){
 		String beforeNonce = timestamp + ":" + transaction + ".";
 		String combineNonce, hash, firstFiveChars;
 		char[] generatedNonce = new char[4];
+		int nonceAttempt = 0;
 
 		for(int i = 0; i < 95; i++){
 
@@ -438,7 +550,11 @@ public class BlockChain {
 							hash = Sha1.hash(combineNonce);
 							firstFiveChars = hash.substring(0, Math.min(hash.length(), 5));
 							if(firstFiveChars.equals("00000")){
+								System.out.println("Attempts of generating nonce: " + nonceAttempt);
 								return String.valueOf(generatedNonce[0]) + String.valueOf(generatedNonce[1]) + String.valueOf(generatedNonce[2]) + String.valueOf(generatedNonce[3]);
+							}
+							else{
+								nonceAttempt++;
 							}
 						}
 						catch(UnsupportedEncodingException e){
@@ -448,7 +564,6 @@ public class BlockChain {
 				}
 			}
 		}
-
 		return null;
 	}
 }
